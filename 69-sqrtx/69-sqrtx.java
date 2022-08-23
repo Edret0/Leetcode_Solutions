2ded68fe2d2854ce0fdf885051fca68101658fcd
Edret0 <90730121+Edret0@users.noreply.github.com>
2022-08-23 17:23:22 -0400

    Time: 78 ms (5.18%), Space: 41.4 MB (45.68%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int mySqrt(int x) {
+        
+        if(x <= 1){
+            return x;
+        }
+        for(long i = 0; i < x; i++){
+            if(i * i == x){
+                return (int)i;
+            }
+            else if((i + 1) * (i + 1) > x){
+                return (int)i;
+            }
+        }
+        return x;
+    }
+}
